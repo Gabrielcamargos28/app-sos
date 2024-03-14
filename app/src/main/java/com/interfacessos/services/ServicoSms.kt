7d005813d8eb0002d2e8a66b95c4afa76fb17bb2
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2024-03-14 15:32:32 -0300

    commit dev

--- app/src/main/java/com/interfacessos/services/ServicoSms.kt
+++ app/src/main/java/com/interfacessos/services/ServicoSms.kt
@@ -2,7 +2,9 @@ package com.interfacessos.services
 
 import android.Manifest
 import android.app.Activity
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.telephony.SmsManager
 import android.widget.Toast
@@ -15,7 +17,13 @@ class ServicoSms{
             val permissao = Manifest.permission.SEND_SMS
             if(ContextCompat.checkSelfPermission(context, permissao) == PackageManager.PERMISSION_GRANTED){
                 val smsManager: SmsManager = SmsManager.getDefault()
-                smsManager.sendTextMessage(telefone, null, mensagem,null,null)
+                val envioIntent = PendingIntent.getBroadcast(context,0, Intent("SMS_ENVIADO"),
+                    PendingIntent.FLAG_IMMUTABLE)
+                val entregueIntent = PendingIntent.getBroadcast(context, 0, Intent("SMS_ENTREGUE"),
+                    PendingIntent.FLAG_IMMUTABLE)
+
+                smsManager.sendTextMessage(telefone, null, mensagem,envioIntent,entregueIntent)
+
                 Toast.makeText(context, "Mensagem enviada com sucesso", Toast.LENGTH_SHORT).show()
             }else{
                 ActivityCompat.requestPermissions(context as Activity, arrayOf(permissao), 0)
